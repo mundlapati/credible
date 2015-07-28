06991e999742598c17e0358ae94c9dd1e3230560
Mundlapati Jawahar <mundlapati@gmail.com>
2015-07-28 09:47:14 +0530

    Update home.php
    gzopen

--- home.php
+++ home.php
@@ -7,38 +7,37 @@
 <meta charset="utf-8"/>
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui">
-<meta name="description" content="100 trillion web addresses">
+<meta name="description" content="Credible sources">
 <meta name="apple-mobile-web-app-capable" content="yes">
-<meta name="keywords" content="nagaiah, nagaiah.com, 100 trillion, web addresses, credible, search index, open source">
+<meta name="keywords" content="nagaiah, nagaiah.com, credible sources, people, products, services, search index, open source">
 <meta name="mobile-web-app-capable" content="yes">
 		
-<title>100 Trillion Credible Web Addresses</title>
+<title>Credible sources</title>
 
 <center><b>
-<code style="font:24px Calibri, Verdana, Arial;color:#222222">100 </code> 
-<code style="font:24px Calibri, Verdana, Arial;color:#545454">Trillion </code>
-<code style="font:24px Calibri, Verdana, Arial;color:#222222">Credible </code> 
-<code style="font:24px Calibri, Verdana, Arial;color:#545454">Web </code> 
-<code style="font:24px Calibri, Verdana, Arial;color:#222222">Addresses </code>
+<code style="font:28px Calibri, Verdana, Arial;color:#222222">Credible sources</code> 
 </b></center>
 <center><b>
-<code style="font:13px Calibri, Verdana, Arial;color:#545454">Last Modified: May 23, 2015 11:35:40 AM UTC</code> 
+<code style="font:13px Calibri, Verdana, Arial;color:#545454">Last Modified: Wed, 22 Jul 2015 08:20:01 GMT</code> 
 </b></center>
 
 <center> 
 <form accept-charset="UTF-8" method="GET" action="home.php"> 
 <p class="inp"> 
-<input name="q" type="text" maxlength="255" autofocus required autocapitalize="off" autocorrect="off" placeholder="site:nagaiah.com iphone"> 
+<input name="q" type="text" maxlength="255" autofocus required autocapitalize="off" autocorrect="off" placeholder="site:apple.com iphone"> 
 </p>
 <input type="submit" value="Search"> 
 </form> 
 </center>
 
 <?php
+$time_start = microtime(true); 
+
 /**
  * 
  * @filename home.php 
- * @author Jawahar Mundlapati mundlapati@gmail.com
+ * @author Jawahar Mundlapati 
+ * @contact mundlapati@gmail.com
  * @license GPL v3 
  * 
 **/
@@ -56,10 +55,6 @@ function get_parse_domain($url)
 function ret_url($req,$reqq) {
 mb_internal_encoding("UTF-8"); mb_http_output("UTF-8"); ob_start("mb_output_handler");
 $strr="<div align=left><ul style=list-style-type:square;margin-top:4px;margin-bottom:3px;padding-left:1px;padding-right:10px;style=font-size:13px;font-family:Calibri, Verdana, Arial;color:#000000;><li><a href=\"".$req."\">".$req."</a></li></ul></div>";
-
-#$strr="<ul style=list-style-type:none;margin-top:4px;margin-bottom:3px;padding-left:1px;padding-right:10px;style=font-size:12px;font-family:Calibri, Verdana, Arial;color:#000000;><li><a href=\"".$req."\">".$req."</a> <a href=\"mailto:admin@nagaiah.com?subject=VERIFY&body=I have paid $1 via http://www.nagaiah.com/paypal.php to VERIFY THE AUTHENTICITY of the opening advertised in $req\" target=\"_blank\"><img style=\"border:0;\" src=\"red_favicon.png\" alt=\"Verify\" width=\"5\" height=\"5\"></a></li></ul>";
-#$r = "<li>".$reqq."</li></ul>";
-#echo $r;
 echo $strr;
 }
 
@@ -79,8 +74,7 @@ function findwords($words, $search)
         $words_array = explode(" ", trim($ll_words));
         $search_array = explode(" ", trim($ll_search));
 
-        if (!array_diff($search_array,$words_array))  return true; else return false;
-        
+        if (!array_diff($search_array,$words_array))  return true; else return false;       
 }
 
 function get_urls($q,$p,$s) {	
@@ -113,90 +107,108 @@ function get_urls($q,$p,$s) {
                 $k = 0;
         }
 
-        $fd = fopen("OUTPUT.txt.5","r");  fseek($fd,$s);
-        $cnt=0; $tags = array();
+  $wiki_tag = "https://en.wikipedia.org/wiki?search=".$q;
+  $fb_tag = "https://facebook.com/search/str/%23".$q."/keywords_top";
+  $twit_tag = "https://twitter.com/search?q=%23".$q;
+  $wiki_hash="<a href=\"".$wiki_tag."\"><img src=_Wikipedia.png></a>";
+  $twit_hash="<a href=\"".$twit_tag."\"><img src=_twitter.png></a>";
+  $fb_hash="<a href=\"".$fb_tag."\"><img src=_facebook.png></a>";
+  echo $twit_hash;echo $wiki_hash;echo $fb_hash;
 
+  //$fd = fopen("d5.txt","r") or die($php_errormsg);  
+  $fd = gzopen64("d59.txt.gz","r") or die($php_errormsg);  
+//  if (fseek($fd,$s,SEEK_SET) === -1) die($php_errormsg);
+  if (gzseek64($fd,$s,SEEK_SET) === -1) die($php_errormsg);
+  $cnt=0; $tags = array();
 
 	while (!feof($fd))
 	{
-	           $mem = fgets($fd); 
-                        $tags = explode(' ',trim($mem),3);
-                        $tags[2] = isset($tags[2]) ? $tags[2] : null;
+//      $mem = fgets($fd) or die($php_errormsg);
+      $mem = gzgets($fd) or die($php_errormsg);
+      $tags = explode(' ',trim($mem),3);
+      $tags[2] = isset($tags[2]) ? $tags[2] : null;
 
-                        if (($tags[2] != null) && ($i == 0)) 
-                        {
-                                $r_domain = get_parse_domain($tags[1]);
+      if (($tags[2] != null) && ($i == 0)) 
+      {
+              $r_domain = get_parse_domain($tags[1]);
 
-                                if (strcasecmp($r_domain, $q_domain) == 0)
-                                {
-                                        if ($j ==0)
-                                        {
-                                                $cnt++;
-                                                if ($cnt <= 10) 
-                                                {
-                                                        ret_url($tags[1],$tags[2]); 
-                                                }
-                                                else 
-                                                {
-                                                        return (round(ftell($fd)));
-                                                }
-                                        }
-                                       
-                                        if (findwords(trim($mem),trim($qp)))
-                                        {
-                                                $cnt++;
-                                                if ($cnt <= 10) 
-                                                {
-                                                        ret_url($tags[1],$tags[2]); 
-                                                }
-                                                else 
-                                                {
-                                                        return (round(ftell($fd)));
-                                                }
-                                        }
-                                }
-                        }
+              if (strcasecmp($r_domain, $q_domain) == 0)
+              {
+                      if ($j ==0)
+                      {
+                              $cnt++;
+                              if ($cnt <= 8) 
+                              {
+                                      unset($mem);
+                                      ret_url($tags[1],$tags[2]); 
+                              }
+                              else 
+                              {
+                                      unset($mem);
+                                      return (round(gztell64($fd)));
+//                                      return (round(ftell($fd)));
+                              }
+                      }
+                     
+                      if (findwords(trim($mem),trim($qp)))
+                      {
+                              $cnt++;
+                              if ($cnt <= 8) 
+                              {
+                                      unset($mem);
+                                      ret_url($tags[1],$tags[2]); 
+                              }
+                              else 
+                              {
+                                      unset($mem);
+                                      return (round(gztell64($fd)));
+//                                      return (round(ftell($fd)));
+                              }
+                      }
+              }
+      }
 
-                        if (($tags[2] != null) && ($k == 0)) 
-                        {
-                                        if (findwords(trim($tags[2]),trim($qq)))
-                                        {
-                                                $cnt++;
-                                                if ($cnt <= 10) 
-                                                {
-                                                        ret_url($tags[1],$tags[2]); 
-                                                }
-                                                else 
-                                                {
-                                                        return (round(ftell($fd)));
-                                                }
-                                        }
-                        }
+      if (($tags[2] != null) && ($k == 0)) 
+      {
+          if (findwords(trim($tags[2]),trim($qq)))
+          {
+                  $cnt++;
+                  if ($cnt <= 8) 
+                  {
+                          unset($mem);
+                          ret_url($tags[1],$tags[2]); 
+                  }
+                  else 
+                  {
+                          unset($mem);
+                          return (round(gztell64($fd)));
+//                          return (round(ftell($fd)));
+                  }
+          }
+      }
 	}
+//  fclose($fd) or die($php_errormsg);
+  gzclose($fd) or die($php_errormsg);
 	echo "</ol>";
+  ob_end_flush();
 }
 
-error_reporting(E_ALL | E_NOTICE);
-ini_set('display_errors', 0);
+// Report all errors
+error_reporting(E_ALL);
 ini_set('memory_limit','-1');
 ini_set('max_execution_time',0);
 setlocale(LC_ALL, 'en_US.UTF8');
 
-if(!isset($_GET['q'])) {exit();} else {$q=rawurlencode($_GET['q']);}
+if(!isset($_GET['q'])) {exit();} else {$q=rawurlencode($_GET['q']);if ($q == "%20") {echo  "Sorry, you entered an invalid query. Please try your search again.\n";exit();}}
 if(!isset($_GET['p'])) {$p=1;} else {$p=rawurldecode($_GET['p']);}
 if(!isset($_GET['s'])) {$s=1;} else {$s=rawurldecode($_GET['s']);}
 if ($p==1) 
 {
   $p=$p;
-  $wiki_tag = "https://en.wikipedia.org/wiki?search=".$q;
-  $fb_tag = "https://facebook.com/search/str/%23".$q."/keywords_top";
-  $twit_tag = "https://twitter.com/search?q=%23".$q;
-  $str="<ol style=list-style-type:square;margin-top:4px;style=font-size:10px;font-family:Calibri, Verdana, Arial;color:#222222;>";echo $str;
-  $wiki_hash="<div align=left><ul style=list-style-type:square;margin-top:4px;margin-bottom:3px;padding-left:1px;padding-right:10px;style=font-size:10px;font-family:Calibri, Verdana, Arial;color:#000000;><li><a href=\"".$wiki_tag."\">".$wiki_tag."</a></li></ul></div>";
-  $fb_hash="<div align=left><ul style=list-style-type:square;margin-top:4px;margin-bottom:3px;padding-left:1px;padding-right:10px;style=font-size:10px;font-family:Calibri, Verdana, Arial;color:#000000;><li><a href=\"".$fb_tag."\">".$fb_tag."</a></li></ul></div>";
-  $twit_hash="<div align=left><ul style=list-style-type:square;margin-top:4px;margin-bottom:3px;padding-left:1px;padding-right:10px;style=font-size:10px;font-family:Calibri, Verdana, Arial;color:#000000;><li><a href=\"".$twit_tag."\">".$twit_tag."</a></li></ul></div>";
-  echo $twit_hash;echo $wiki_hash;echo $fb_hash;
-  echo "</ol>";
+ echo "<!-- filename=home.php -->\n";
+ echo "<!-- author=Jawahar Mundlapati -->\n";
+ echo "<!-- contact=mundlapati@gmail.com -->\n";
+ echo "<!-- license=GPL v3 -->\n";
 }
 $ret_val = get_urls($q,$p,$s);
 if($ret_val > 1){
@@ -209,17 +221,22 @@ $hne = utf8_rawurldecode($ne);
 $en="<b><a href=\"home.php?q=$q&p=$pp&s=$ss\"style=\"font-size:14px;;letter-spacing:2px;color:#0000BB;border:0px solid #0000BB;margin:1px 1px 1px 70px;\">".$hne."</a></b>";
 print("<p><div align=\"center\">$en</div></p>");
 }
+
+print("<center>");
+print("<code style=\"font:13px Calibri, Verdana, Arial;color:#545454\">"); 
+print("Not better than <a href=\"https://www.google.com/#q=$q&safe=off\">Google </a> and <a href=\"https://www.bing.com/search?q=$q\">Bing</a><br>");
+print("Please <a href=\"mailto:admin@nagaiah.com?Subject=Credible\">let us know</a>");
+print("</code>");
+print("</center>");
 echo "<hr>"; echo "<center>";echo "<br>"; echo "</center>";
+$time_end = microtime(true);
+$execution_time = ($time_end - $time_start)/60;
+echo '<center><b></b></center> '.$execution_time.' Mins';
 ?>
 
 <center>
-<a href="index.php" target="_top">Credible</a> .
-<a href="http://www.gnu.org/copyleft/gpl.html" target="_top">GPL</a> .
-<a href="contributors.html" target="_top">Contributors</a> .
-<a href="http://www.bbc.co.uk/news/technology-23866614" target="_top">Goal</a>
-<div align="center"><a href="mailto:admin@nagaiah.com?Subject=100%20Trillion%20Web%20Addresses" target="_top">Submit Feedback</a><br>
-<div align="center">&copy;2015 nagaiah.com. All rights reserved.
-<div align="center"><?php echo gmdate("Y-m-d H:i:s A");?> UTC
+<a href="index.php" target="_top">Credible solutions</a>
+<div align="center"><?php echo gmdate('D, d M Y H:i:s T', time());?>
 </center>
 
 </body></html> 
